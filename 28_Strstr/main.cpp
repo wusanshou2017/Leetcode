8e5a31f34e9d4d54a98087e0a1590039e7c1f674
wu_allin <252652905@qq.com>
2018-12-25 23:22:38 +0800

    Add files via upload

---
+++ 28_Strstr/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<string>
+using namespace std;
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        int m=haystack.size();
+        int n=needle.size();
+        int ans=-1;
+        int temp=0;
+        for(int j=0;j<m;++j)
+        {
+            if(haystack[j]==needle[0])
+            {
+                ans=0;
+                temp=j;
+                break;
+            }
+        }
+        for(int i=0;i<n;++i)
+        {
+            if(haystack[temp+i]==needle[i])
+            {
+                ans++;
+            }
+            else ans=-1;
+        }
+        return ans;
+    }
+};
+int main()
+{
+    Solution solution;
+    cout<<solution.strStr("hello","ll");
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
