26cb2195d94374b3847cad681cb324007db062ac
wu_allin <252652905@qq.com>
2020-06-25 12:31:05 +0800

    coin_change

---
+++ using_python/322_coin_change/coin_change.py
@@ -0,0 +1,10 @@
+class Solution:
+    def coinChange(self, coins: [int], amount: int) -> int:
+
+    	dp = [0] + [float("inf")] *amount 
+    	for coin in coins:
+    		for i in range (coin , amount+1):
+    			dp[i] = min (dp[i], dp[i-coin]+1)
+
+    	return dp[amount] if dp[amount]!= float("inf") else -1
+
