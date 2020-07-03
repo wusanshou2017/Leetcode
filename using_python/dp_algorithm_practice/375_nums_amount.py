8e93a75cd1824b06c4e827912c29f7e45734d0e7
wu_allin <252652905@qq.com>
2020-07-03 18:18:30 +0800

    K_maxSum to be Sloved

--- using_python/dp_algorithm_practice/375_nums_amount.py
+++ using_python/dp_algorithm_practice/375_nums_amount.py
@@ -10,18 +10,23 @@ class Solution:
 
                 dp[low][high] = min(
                     x + max(dp[low][x - 1], dp[x + 1][high]) for x in range(low, high))
-                print("dp[][]...:", dp[low][high])
-                print("low , high...:", [low, high])
 
         return dp[1][n]
 
     def getAmount(self, n: int) -> int:
+
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
         for i in range(1, n):
             dp[i][i + 1] = i
 
-            pass
+        for i in range(n - 1, 0, -1):
+            for j in range(i + 1, n + 1):
+                dp[i][j] = min(x + max(dp[i][x - 1], dp[x + 1][j])
+                               for x in range(i, j))
+
+        return dp[1][n]
 
 
 test = 0
 so = Solution()
-print(so.test(6))
+print(so.getAmount(5))
