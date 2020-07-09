8179fc144f146e44247c864d6a70412417800f32
wu_allin <252652905@qq.com>
2020-07-09 09:06:12 +0800

    k_maxmin_subarray

--- using_python/K_maxSum.py
+++ using_python/K_maxSum.py
@@ -1,2 +1,33 @@
 class Solution:
-    def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
+    def kConcatenationMaxSum(self, arr: [int], k: int) -> int:
+        s, ans = 0, 0
+        for a in arr * min(2, k):
+            s = a if s < 0 else s + a
+            if s > ans:
+                ans = s
+        if k <= 2:
+            return ans
+
+        return (max(sum(arr), 0) * (k - 2) + ans) % 1000000007
+
+    def test(self, arr: [int], k: int) -> int:
+        n = len(arr)
+        s, d = [0] * (n + 1), [0] * n
+        for i in range(n):
+            s[i + 1] = s[i] + arr[i]
+            d[i] = max(d[i - 1] + arr[i], 0)
+
+        print(s)
+        print(d)
+        if k == 1:
+            return max(d)
+
+        return max(max(d), max(s[n], 0) * (k - 2) + s[n] - min(s) + max(s))
+
+
+so = Solution()
+
+print(so.test([-1, 1, 2], 5))
+
+
+print("lll")
