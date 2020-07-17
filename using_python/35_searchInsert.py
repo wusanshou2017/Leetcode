2c164ce15727fa71e685bf19c98c17cef64d834c
wu_allin <252652905@qq.com>
2020-07-17 16:39:55 +0800

    searchInsert

---
+++ using_python/35_searchInsert.py
@@ -0,0 +1,33 @@
+
+# 输入: [1,3,5,6], 5
+# 输出: 2
+
+# 输入: [1,3,5,6], 2
+# 输出: 1
+
+class Solution:
+    def searchInsert(self, nums: [int], target: int) -> int:
+        n = len(nums)
+        l = 0
+        r = n - 1
+        if target > nums[r]:
+            return n
+        if target <= nums[l]:
+            return 0
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] > target:
+                r = mid - 1
+
+            if nums[mid] < target:
+                l = mid + 1
+
+            if nums[mid] == target:
+                return mid
+
+        return l
+
+
+so = Solution()
+
+print(so.searchInsert([1, 3, 5, 6], 1))
