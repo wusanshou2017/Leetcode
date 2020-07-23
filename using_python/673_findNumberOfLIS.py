d0c3dbad99a386bf83fef538f230b8e00cc74c0c
wu_allin <252652905@qq.com>
2020-07-23 10:59:22 +0800

    LIS solved

--- using_python/673_findNumberOfLIS.py
+++ using_python/673_findNumberOfLIS.py
@@ -4,25 +4,26 @@
 # 输出: 5
 
 class Solution:
-	def findNumberOfLIS(self, nums: [int]) -> int:
-		if not nums:
-			return 0
-		n= len(nums)
-		length = [1]*n
-		counter =[1]*n
+    def findNumberOfLIS(self, nums: [int]) -> int:
+        if not nums:
+            return 0
+        n = len(nums)
+        length = [1] * n
+        counter = [1] * n
 
-		for i in range (1,n):
-			for j in range(i):
-				if nums[j]<nums[i]:
-					if length[j]+1 >length[i]:
-						length[i] =length[j]+1
-						counter[i] =length[j]
-					elif length[j]+1 ==length[i]:
-						counter[i] =counter[i]+ counter[j]
-		temp =max(length)
-		res =sum([counter[i] for i in range(n) if length[i] ==temp ])
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if length[j] + 1 > length[i]:
+                        length[i] = length[j] + 1
+                        counter[i] = counter[j]
+                    elif length[j] + 1 == length[i]:
+                        counter[i] = counter[i] + counter[j]
+        temp = max(length)
+        res = sum([counter[i] for i in range(n) if length[i] == temp])
 
-		return res 
+        return res
 
-so =Solution()
-print (so.findNumberOfLIS([1,3,5,4,7]))
+
+so = Solution()
+print(so.findNumberOfLIS([1, 3, 5, 4, 7]))
