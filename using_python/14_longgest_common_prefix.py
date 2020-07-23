902d12f40aa48ea5e86a4241ba51f986069283f4
wu_allin <252652905@qq.com>
2020-07-23 17:14:27 +0800

    14

--- using_python/14_longgest_common_prefix.py
+++ using_python/14_longgest_common_prefix.py
@@ -1,23 +1,34 @@
 class Solution:
     def longestCommonPrefix(self, strs: [str]) -> str:
-        l_lst = [len(item) for item in strs]
-        min_len = min(l_lst)
-        n = len(strs)
-        end_index = 0
-        for i in range(n):
-            for j in range(min_len):
-                if strs[i][j] == strs[0][j]:
-                    end_index = j
-                else:
-                    ans = strs[0][:j]
-                    return ans
 
-        return strs[0][:end_index + 1]
+        if not strs:
+            return ""
+        if "" in strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+
+        nums_str = len(strs)
+        end_index = len(strs[0])
+        for item in strs[1:]:
+            end_index = min(end_index, self.compare_two_str(strs[0], item))
+
+        return strs[0][:end_index]
+
+    def compare_two_str(self, str1, str2):
+        n = min(len(str1), len(str2))
+        index = 0
+        for i in range(n):
+            if str1[i] != str2[i]:
+                break
+            else:
+                index += 1
+        return index
 
 
 so = Solution()
 
-test = ["appll", "abp", "apple"]
+test = ["appll", "app", "apple"]
 
 
 print(so.longestCommonPrefix(test))
