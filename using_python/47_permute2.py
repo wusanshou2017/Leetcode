f6f86ca58e405b0736c80109133bda1fdcf02516
wu_allin <252652905@qq.com>
2020-08-10 16:21:22 +0800

    solve_47_premute2

--- using_python/47_permute2.py
+++ using_python/47_permute2.py
@@ -1,19 +1,30 @@
 class Solution:
     def premute(self, nums):
-        def backtrack(first=0):
-            if first == n:
-                res.append(nums[:])
-            visited = set()
-            for i in range(first, n):
-                if nums[i] in visited:
+        def dfs(nums, size, depth, path, used, res):
+            if depth == size:
+                res.append(path[:])
+                return res
+
+            for i in range(size):
+                if not used[i]:
+                    if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                        continue
+                    used[i] = True
+                    path.append(nums[i])
+                    dfs(nums, size, depth + 1, path, used, res)
+                    used[i] = False
+                    path.pop()
+
+                else:
                     continue
-                nums[first], nums[i] = nums[i], nums[first]
-                visited.add(nums[i])
-                backtrack(first + 1)
-                nums[i], nums[first] = nums[first], nums[i]
-        n = len(nums)
+
+        size = len(nums)
+        if size == 0:
+            return []
+        used = [False for _ in range(size)]
+        nums.sort()
         res = []
-        backtrack()
+        dfs(nums, size, 0, [], used, res)
         return res
 
 
