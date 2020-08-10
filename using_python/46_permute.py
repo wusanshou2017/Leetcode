cc2b8c62e1c24a055e07325622ab36a0600d3323
wu_allin <252652905@qq.com>
2020-08-10 15:47:44 +0800

    add some solution

--- using_python/46_permute.py
+++ using_python/46_permute.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def permute(self, nums):
         """
@@ -20,3 +23,34 @@ class Solution:
         res = []
         backtrack()
         return res
+
+    def permute_dfs(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, size, depth, path, used, res):
+            if depth == size:
+                print(path)
+                res.append(path[:])
+                return
+
+            for i in range(size):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+
+                    dfs(nums, size, depth + 1, path, used, res)
+
+                    used[i] = False
+                    path.pop()
+
+        size = len(nums)
+        if len(nums) == 0:
+            return []
+
+        used = [False for _ in range(size)]
+        res = []
+        dfs(nums, size, 0, [], used, res)
+        return res
+
+
+so = Solution()
+
+print(so.permute_dfs([1, 2, 3]))
