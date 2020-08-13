07396768e448faee39c240d0ff8207247a6d8d20
wu_allin <252652905@qq.com>
2020-08-13 11:55:55 +0800

    add Cplusplus

---
+++ using_C++/46_permute/cmake-build-debug/CMakeFiles/46_permute.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/46_permute.dir/main.cpp.obj"
+  "46_permute.pdb"
+  "46_permute.exe"
+  "46_permute.exe.manifest"
+  "lib46_permute.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/46_permute.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ using_C++/46_permute/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using  namespace std;
+class Solution{
+    public:
+        vector<vector <int>> permute(vector<int> nums){
+            int n = nums.size();
+            if (n==0){
+                return {};
+            }
+            vector<bool > used(n, false);
+            vector<vector <int>> res;
+            vector <int > path;
+            dfs(nums,n,0,path,used,res);
+            return res;
+        }
+        void dfs (vector<int> nums,int size,int depth ,vector<int > &path,vector<bool > &used ,vector<vector<int >> &res){
+            if (depth== size){
+                res.push_back(path);
+                return ;
+            }
+            for (int i=0;i<size;i++){
+                if (!used[i]){
+                    used[i] =true;
+                    path.push_back(nums[i]);
+                    dfs(nums,size,depth+1,path,used,res);
+                    used[i] =false;
+                    path.pop_back();
+                }
+
+            }
+        }
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
