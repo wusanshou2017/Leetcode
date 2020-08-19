0ef455e73a86ef31aac07aadbc493486e24be959
wu_allin <252652905@qq.com>
2020-08-19 17:34:51 +0800

    647_solved

--- using_python/50_mypow.py
+++ using_python/50_mypow.py
@@ -1,9 +1,13 @@
 class Solution:
-	def myPow(self,x:float,n:int)->float:
-		def quick_mul(N):
-			if N ==0:
-				return 1.0
-			y =quick_mul(N//2)
-			return y*y if N %2==0 else y*y*x
+    def myPow(self, x: float, n: int) -> float:
+        def quick_mul(N):
+            if N == 0:
+                return 1.0
+            y = quick_mul(N // 2)
+            return y * y if N % 2 == 0 else y * y * x
 
-		return quick_mul(n) if n>=0 else 1.0/quick_mul(-n)
+        return quick_mul(n) if n >= 0 else 1.0 / quick_mul(-n)
+
+
+so = Solution()
+print(so.myPow(3, 4))
---
+++ using_python/647_countSubstrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(i + 1):
+                if s[i] == s[j] and (i - j < 2 or dp[j + 1][i - 1]):
+                    dp[j][i] = 1
+                    res += 1
+
+        return res
