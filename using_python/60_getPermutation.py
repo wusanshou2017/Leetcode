6d902067a63380b031577f38b54b04dbbac21838
wu_allin <252652905@qq.com>
2020-08-25 23:02:03 +0800

    60_permutation

--- using_python/60_getPermutation.py
+++ using_python/60_getPermutation.py
@@ -10,18 +10,29 @@
 # "321"
 
 class Solution:
-    def getPermutation(self, nums: [int]):
-        res = []
+    def getPermutation(self, n: int, k: int) -> str:
+        factorials, nums = [1], ['1']
+        for i in range(1, n):
+            # generate factorial system bases 0!, 1!, ..., (n - 1)!
+            factorials.append(factorials[i - 1] * i)
+            # generate nums 1, 2, ..., n
+            nums.append(str(i + 1))
+        
+        # fit k in the interval 0 ... (n! - 1)
+        k -= 1
+        
+        # compute factorial representation of k
+        output = []
+        for i in range(n - 1, -1, -1):
+            idx = k // factorials[i]
+            k -= idx * factorials[i]
+            
+            output.append(nums[idx])
+            del nums[idx]
+        
+        return ''.join(output)
 
-        def backtrack(nums, tmp):
-            if not nums:
-                res.append(tmp)
-                return
-            for i in range(len(nums)):
-                backtrack(nums[:i] + nums[i + 1:], tmp + [nums[i]])
-        backtrack(nums, [])
-        return res
 
 
 so = Solution()
-print(so.getPermutation([1, 2, 3]))
+print(so.getPermutation(3,2))
