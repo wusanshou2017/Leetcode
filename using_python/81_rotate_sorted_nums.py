e16353f33259ed2f0b716d5abfe2ed9a91ed8d7c
wu_allin <252652905@qq.com>
2020-08-30 16:45:37 +0800

    81_search_rotate_nums

---
+++ using_python/81_rotate_sorted_nums.py
@@ -0,0 +1,17 @@
+# input:nums = [2,5,6,0,0,1,2], target = 0
+# output:True
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+
+    	n=len(nums)
+    	l =0
+    	r =n-1
+    	while l<r:
+    		mid =(l+r)//2
+    		if nums[l]<nums[mid]:
+
+    		else:
+
+    			
+
