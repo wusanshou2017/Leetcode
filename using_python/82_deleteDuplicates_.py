dcba1522a52f73a187f77b43218b771f9c9b9198
wu_allin <252652905@qq.com>
2020-09-01 21:47:01 +0800

    82_solved

--- using_python/82_deleteDuplicates_.py
+++ using_python/82_deleteDuplicates_.py
@@ -7,9 +7,18 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        if not head:
-            return head
+        dummy = pre =ListNode(0)
+        dummy.next =head
 
-        pre = head
-        cur = head.next
-        while pre is not None and cur is not None:
+        while head and head.next:
+        	if head.val == head.next.val:
+        		while head and head.next and head.val == head.next.val:
+        			head =head.next
+        		head =head.next
+        		pre.next =head 
+
+        	else:
+        		head = head.next
+        		pre = pre.next
+        return dummy.next
+
