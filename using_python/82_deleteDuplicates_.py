cbba25d5ade26db9802be9b7049431c4d6976bcb
wu_allin <252652905@qq.com>
2020-09-01 18:58:50 +0800

    82_to_be_solved

---
+++ using_python/82_deleteDuplicates_.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        pre = head
+        cur = head.next
+        while pre is not None and cur is not None:
