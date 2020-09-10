dd1428b8e0a53f1c74f4a07d664dfcf50435a104
wu_allin <252652905@qq.com>
2020-09-10 12:11:56 +0800

    add using_go

---
+++ using_Go/86_partial.go
@@ -0,0 +1,32 @@
+package partition
+ // Definition for singly-linked list.
+ 
+import "fmt"
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+
+
+func partition(head *ListNode, x int) *ListNode {
+    dummy1 :=&ListNode{}
+    dummy2 :=&ListNode{}
+    dummy1_head := dummy1
+    dummy2_head :=dummy2
+    for head!= nil{
+        if head.Val<x{
+            dummy1.Next=head
+            dummy1 = dummy1.Next
+        } else{
+            dummy2.Next =head
+            dummy2 = dummy2.Next
+        }
+        head = head.Next
+    }
+
+    dummy2.Next =nil 
+    dummy1.Next = dummy2_head.Next
+    return dummy1_head.Next
+}
