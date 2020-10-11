fd3da2e785fc70179e70dfeb1df54779f3cf0997
wu_allin <252652905@qq.com>
2020-10-11 15:20:04 +0800

    buildTree

---
+++ using_python/binary_tree/从中序和后序遍历构造二叉树.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+    	if not inorder or not postorder:
+            return 
+        root = TreeNode(postorder[-1])
+        idx = inorder.index(postorder[-1])
+        root.left = self.buildTree(inorder[:idx],postorder[:idx])
+        root.right = self.buildTree(inorder[idx+1:],postorder[idx:-1])
+        return root
+
