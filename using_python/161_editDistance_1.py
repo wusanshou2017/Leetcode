9d23f2dafa7a9ae891446279d27acbb80facf5b6
wu_allin <252652905@qq.com>
2020-10-21 15:41:41 +0800

    138_to_be_solved

--- using_python/161_editDistance_1.py
+++ using_python/161_editDistance_1.py
@@ -2,11 +2,11 @@ class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
         m = len(s)
         n = len(t)
-        if m -n >1 or n-m>1:
+        if m - n > 1 or n - m > 1:
             return False
-            
+
         dp = [[i + j for i in range(n + 1)]for j in range(m + 1)]
-        
+
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if s[i - 1] == t[j - 1]:
@@ -15,7 +15,7 @@ class Solution:
                     d = 1
                 dp[i][j] = min(dp[i - 1][j] + 1, dp[i]
                                [j - 1] + 1, dp[i - 1][j - 1] + d)
-     
+
         return dp[m][n] == 1
 
 
