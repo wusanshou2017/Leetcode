ed89d74f1726d04c6b169bdd4fca2a7b3d9c8fe2
wu_allin <252652905@qq.com>
2020-10-27 19:44:36 +0800

    max_length

--- using_python/5_max_length.py
+++ using_python/5_max_length.py
@@ -1,20 +1,39 @@
 class Solution:
+    # def longestPalindrome(self, s: str) -> str:
+    #     n = len(s)
+    #     if n < 2:
+    #         return s
+    #     res = ""
+    #     for j in range(0, n + 1):
+    #         for i in range(j):
+
+    #             if s[i:j] == s[j:i:-1]:
+    #                 if j - i > len(res):
+    #                     res = s[i:j]
+    #     return res
+
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
         if n < 2:
             return s
         res = ""
-        for j in range(0, n + 1):
-            for i in range(j):
+        for i in range(n):
+            left, right = 0, 0
+            while i - left >= 0 and i + right <= n - 1:
+                if self.check(s[i - left:i + right + 1]):
+                    if len(s[i - left:i + right + 1]) > len(res):
+                        res = s[i - left:i + right + 1]
+                elif self.check(s[i - left:i + right + 2]):
+                    if len(s[i - left:i + right + 2]) > len(res):
+                        res = s[i - left:i + right + 2]
+                left += 1
+                right += 1
 
-                if self.check(s[i:j]):
-                    if j - i > len(res):
-                        res = s[i:j]
-        return res
+        return res if res != "" else s[0]
 
     def check(self, s: str):
         return s[:] == s[::-1]
 
 
 so = Solution()
-print(so.longestPalindrome(""))
+print(so.longestPalindrome("cbbd"))
