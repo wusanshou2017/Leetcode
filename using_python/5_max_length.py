2e1f391d31e882b1ac007e1a28d5c0add4221f23
wu_allin <252652905@qq.com>
2020-10-30 14:15:17 +0800

    some change

--- using_python/5_max_length.py
+++ using_python/5_max_length.py
@@ -14,22 +14,15 @@ class Solution:
 
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        if n < 2:
-            return s
-        res = ""
-        for i in range(n):
-            left, right = 0, 0
-            while i - left >= 0 and i + right <= n - 1:
-                if self.check(s[i - left:i + right + 1]):
-                    if len(s[i - left:i + right + 1]) > len(res):
-                        res = s[i - left:i + right + 1]
-                elif self.check(s[i - left:i + right + 2]):
-                    if len(s[i - left:i + right + 2]) > len(res):
-                        res = s[i - left:i + right + 2]
-                left += 1
-                right += 1
+        dp = [[False] * n for _ in range(n)]
+        for i in range (n):
+            dp[i][i] =True
+        for i in range (n):
+            for j in range ():
+                dp[i][j] = (s[i]==s[j] and dp[i-1][j-1]):
+                if dp[]
 
-        return res if res != "" else s[0]
+
 
     def check(self, s: str):
         return s[:] == s[::-1]
