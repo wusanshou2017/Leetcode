7ce0745db0082e563bd4a6607317f30e7d2c2964
wu_allin <252652905@qq.com>
2020-11-12 13:37:03 +0800

    31 to be solved

---
+++ using_python/31_nextPermute.py
@@ -0,0 +1,9 @@
+# 1,2,3 → 1,3,2
+# 3,2,1 → 1,2,3
+# 1,1,5 → 1,5,1
+
+class Solution():
+    """docstring for ClassName"""
+    def nextPermute(self, nums:[]):
+        i, j = 0, len(nums)-1
+        while  
