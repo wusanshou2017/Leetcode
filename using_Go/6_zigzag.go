1292c05151508e6b91a925b8d044fb7b2a267c8a
wu_allin <252652905@qq.com>
2020-11-17 21:22:28 +0800

    6 zigzag

---
+++ using_Go/6_zigzag.go
@@ -0,0 +1,16 @@
+import(
+    "strings"
+)
+
+func convert(s string, numRows int) string {
+    if (numRows<2){return s}
+    res  := make([]string,numRows)
+    i:=0
+    flag :=-1
+    for _,c:=range s{
+        res[i]+=string(c)
+        if (i==0 || i ==numRows-1){flag=-flag}
+        i+=flag
+    }
+    return strings.Join(res,"")
+}
