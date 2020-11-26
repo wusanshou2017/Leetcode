8fa3fef7084540d826d1ebead1059f593f2fdf85
wu_allin <252652905@qq.com>
2020-11-26 18:21:38 +0800

    some flaws in my code

---
+++ using_python/11_most_water.py
@@ -0,0 +1,23 @@
+#  [1,8,6,2,5,4,8,3,7]
+#  49
+
+class Solution:
+    def solve_most(self, nums: [int]) -> int:
+        left, right = 0, len(nums) - 1
+        ans = 0
+
+        while left < right:
+            if nums[left] > nums[right]:
+                ans = max((right - left) * nums[right], ans)
+                right -= 1
+
+            elif nums[left] < nums[right]:
+                ans = max((right - left) * nums[left], ans)
+                left += 1
+
+        return ans
+
+
+so = Solution()
+
+print(so.solve_most([8, 8, 8, 8, 8]))
