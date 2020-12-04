c198369ee5f5d56fae2215be50be914f6bfa0809
wu_allin <252652905@qq.com>
2020-12-04 18:01:46 +0800

    should be solved by Go language

---
+++ using_Go/3sum.go
@@ -0,0 +1,20 @@
+package main
+import {
+    "fmt"
+    "sort"
+}
+func three_sum(nums[] int) int{
+    sort.Ints(nums)
+    res:= make([],100)
+    dic_sum_index = make(map[[]] int)
+    // loop for creat the map
+    
+
+
+}
+
+func main() {
+
+    
+}
+
