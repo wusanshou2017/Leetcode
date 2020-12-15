53e5a35087c63acbc888fc2d839b20b2bab89ba2
wu_allin <252652905@qq.com>
2020-12-15 18:31:50 +0800

    to be done

--- using_python/increasing.py
+++ using_python/increasing.py
@@ -1,38 +1,37 @@
 class Solution():
-    def findIncreaseNum(self,num:int)->int:
+    def findIncreaseNum(self, num: int) -> int:
         temp_str = str(num)
         n = len(temp_str)
         flag_increase = True
-        for i in range(1,n):
-            if int(temp_str[i])>=int(temp_str[i-1]):
+        for i in range(1, n):
+            if int(temp_str[i]) >= int(temp_str[i - 1]):
                 continue
             else:
-                flag_increase =False
+                flag_increase = False
                 break
 
         if flag_increase:
             return num
-
-        temp_num =num-1
+        temp_num = num
         while not flag_increase:
-            print("...")
-            temp_str =str(temp_num)
+            temp_num -= 1
+            temp_str = str(temp_num)
             n = len(temp_str)
-            flag_temp =True
-            for i in range(1,n):
-                if int(temp_str[i])>=int(temp_str[i-1]):
+            flag_temp = True
+            for i in range(1, n):
+                if int(temp_str[i]) >= int(temp_str[i - 1]):
                     continue
                 else:
-                    flag_temp=False
+                    flag_temp = False
                     break
-            temp_num-=1
             if flag_temp:
                 break
-
         return temp_num
 
 def unit_test():
 
-    so =Solution()
-    print(so.findIncreaseNum(130))
-           
+    so = Solution()
+    print(so.findIncreaseNum(99999999))
+
+
+unit_test()
