8b5c872542415f267ce124baeac013c1e0178205
wu_allin <252652905@qq.com>
2020-12-22 18:10:35 +0800

    golang implement treeNode

--- using_python/103_zigzag_travle_tree.py
+++ using_python/103_zigzag_travle_tree.py
@@ -1,45 +1,61 @@
 # Definition for a binary tree node.
-from typing import List
-from collections import deque
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
+
+from collections import deque
+
+
 class Solution:
+    # this just based on the level order travel ,just reverse the node_val
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
-            return [[]]
+            return []
 
         flag_level = True
 
-        result,queue = [], deque([root])
+        result, queue = [], deque([root])
         while queue:
-            level_len =len(queue)
+            level_len = len(queue)
             level_nodes = []
             while level_len > 0:
-                
-                cur_node = queue.popleft() 
+
+                cur_node = queue.popleft()
                 level_nodes.append(cur_node.val)
-               
+
                 if cur_node.right:
                     queue.append(cur_node.right)
                 if cur_node.left:
-                    queue.append(cur_node.left)                    
-                
-                level_len-=1
-            if flag_level:
+                    queue.append(cur_node.left)
+
+                level_len -= 1
+            if not flag_level:
                 result.append(level_nodes[:])
             else:
                 result.append(level_nodes[::-1])
             flag_level = not flag_level
         return result
 
+    def layer_order(self,root:TreeNode) ->List[List[int]]:
+        if not root:
+            return []
 
-# input : [3,9,20,null,null,15,7]
-# expect_output: [[3],[20,9],[15,7]]
+        result,queue = [],deque([root])
+        while queue:
+            level_len = len(queue)
+            level_nodes =[]
+            while level_len>0:
+                cur_node = queue.popleft()
+                level_nodes.append(cur_node.val)
+                if cur_node.left:
+                    queue.append(cur_node.left)
 
+                if cur_node.right:
+                    queue.append(cur_node.right)
+                level_len-=1
+            result.append(level_nodes)
 
-
-
+        return result
