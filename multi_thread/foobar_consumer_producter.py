82f8181fc8cd4c15272f74998dc01ec2f3993aa9
wu_allin <252652905@qq.com>
2020-12-28 14:55:23 +0800

    multipy_thread

--- multi_thread/foobar_consumer_producter.py
+++ multi_thread/foobar_consumer_producter.py
@@ -8,24 +8,27 @@ q = Queue(maxsize=1)
 class FooBar:
     def __init__(self, n):
         self.n = n
-        self.q = Queue(1)
+        self.q1 = Queue(1)
+        self.q2 = Queue(1)
+        self.q1.put(0)
 
     def foo(self) -> None:
 
         for i in range(self.n):
 
             # printFoo() outputs "foo". Do not change or remove this line.
-            self.q.put(1)
+            self.q1.get()
             # self.printFoo()
             print("foo")
+            self.q2.put(0)
 
     def bar(self) -> None:
 
         for i in range(self.n):
-            self.q.get()
+            self.q2.get()
             # self.printBar()
             print("bar")
-           
+            self.q1.put(0)
 
     def printFoo(self):
         print("foo")
