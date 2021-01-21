ad8e92f45ed504544156b1aae83691acb82514fa
wu_allin <252652905@qq.com>
2021-01-21 10:27:38 +0800

    brutal force to solve 628

--- using_python/628_maxThreeProduct.py
+++ using_python/628_maxThreeProduct.py
@@ -14,22 +14,10 @@ class Solution:
             else:
                 pos_lst.append(num)
 
-        if len(nums) == 3:
-            return nums[0] * nums[1] * nums[2]
-
-        if len(neg_lst) >= 2:
-            if len(pos_lst) > 2:
-                return max(neg_lst[0] * neg_lst[1] * pos_lst[-1], pos_lst[-2] * pos_lst[-1] * pos_lst[-3])
-
-            return neg_lst[0] * neg_lst[1] * pos_lst[-1]
-
-        else:
-            if len(pos_lst) < 3:
-                return 0
-            else:
-                return pos_lst[-2] * pos_lst[-1] * pos_lst[-3]
+        return max(nums[0]*nums[1]*nums[-1], nums[-3]*nums[-2]*nums[-1]) 
 
 
 so = Solution()
 
 print(so.maximumProduct([-100, -98, -1, 2, 3, 4]))
+
