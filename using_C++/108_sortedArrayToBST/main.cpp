9685a246968937e23833874435179a62b3163057
wu_allin <252652905@qq.com>
2021-02-01 18:57:25 +0800

    108 C++ and go solved

--- using_C++/108_sortedArrayToBST/main.cpp
+++ using_C++/108_sortedArrayToBST/main.cpp
@@ -9,6 +9,7 @@ struct TreeNode{
     TreeNode(int x):val(x),left(NULL),right(NULL){}
 };
 //[-10,-3,0,5,9] l:0,r:4
+//[0,-10,-3,5,9]
 class Solution{
 public:
     TreeNode *helper(int left ,int right,vector<int >& nums){
@@ -17,7 +18,7 @@ public:
         cout <<"the_mid:.."<<mid<<endl;
         TreeNode *root =new TreeNode(nums[mid]);
         root->left = helper(left,mid-1,nums); // (0,1)
-        root->right = helper(mid+1,right ,nums);//(3,4)
+        root->right = helper(mid+1,right ,nums);// (3,4)
         return root;
     }
 
