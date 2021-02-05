04ea40934e19d3a0970e8dacc0a5efd26f4176d0
wu_allin <252652905@qq.com>
2021-02-05 10:58:18 +0800

    go random copy Listnode

--- using_Go/138_randomNodecopy.go
+++ using_Go/138_randomNodecopy.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "fmt"
-    "os"
 )
 
 type Node struct {
@@ -18,7 +17,7 @@ func getCloneNode(visitedMap map[*Node] *Node, p *Node) *Node{
     }else{
         var v* Node
         if p != nil{
-            v= &Node{p.val,nil,nil}
+            v= &Node{p.Val,nil,nil}
         }
         visitedMap[p]= v
         return v
@@ -34,10 +33,39 @@ func copyRandomList(head *Node) *Node{
     newNode := getCloneNode( visitedMap,oldNode)
     newHead := newNode
     for oldNode != nil{
-        newNode.next =getCloneNode(visitedMap, oldNode.Next)
+        newNode.Next =getCloneNode(visitedMap, oldNode.Next)
         newNode.Random = getCloneNode(visitedMap, oldNode.Random)
         oldNode = oldNode.Next
         newNode = newNode.Next
     }
     return newHead
+}
+
+// unit test 
+// data 1 [2]->2 [self] ->None
+
+
+func main() {
+    // generate test data
+    node1 :=&Node{}
+    node2 :=&Node{}
+    node1.Val = 1
+    node1.Next =node2
+    node2.Val = 2
+    node2.Next =nil
+    node1.Random =node2
+    node2.Random = node1
+    res:= copyRandomList( node1)
+    temp1 := res
+    for temp1!=nil{
+        fmt.Println("next 域:...",temp1.Val)
+        temp1 =temp1.Next
+    }
+    
+    temp2:=res 
+    for temp2!=nil{
+        fmt.Println("random 域:...",temp2.Val)
+        temp2=temp2.Random
+    }
+
 }
