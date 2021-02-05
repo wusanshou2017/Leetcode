45342065a3425de33f4cfefe881fdda710635caf
wu_allin <252652905@qq.com>
2021-02-05 09:36:13 +0800

    别傻了，换条别的路吧

--- using_python/19_removeNthfromEnd.py
+++ using_python/19_removeNthfromEnd.py
@@ -109,24 +109,3 @@ class Solution:
 
         return dummy.next
 
-
-head = ListNode(0)
-node1 = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(3)
-node4 = ListNode(4)
-
-head.next = node1
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = None
-
-so = Solution()
-
-
-res = so.removeNthFromEnd(head, 2)
-while res:
-
-    print(res.val)
-    res = res.next
