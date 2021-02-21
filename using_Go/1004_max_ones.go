b7116e69f6740debd643dff21d2143998584dcff
wu_allin <252652905@qq.com>
2021-02-21 13:41:44 +0800

    1004 solved

--- using_Go/1004_max_ones.go
+++ using_Go/1004_max_ones.go
@@ -1,5 +1,7 @@
 package main
-
+import (
+    "fmt"
+)
 func max_int(a int ,b int ) int{
     if (a>b){
         return  a 
@@ -16,17 +18,15 @@ func longestOnes(A []int, K int) int {
     n := len(A)
     for slow<n && fast<n {
         if (A[fast]>0 || K>0){
-            if A[fast]>0{
+            if A[fast]==0{
                 K--
-            }else{
-                fast++
             }
+            fast++
         }else{
             if (A[slow]==0){
                 fast++
-            }else{
-                slow++
             }
+            slow++
         }
         res = max_int(res,fast-slow)
     }
@@ -35,4 +35,6 @@ func longestOnes(A []int, K int) int {
 
 func main() {
     
+    test_data :=[] int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}
+    fmt.Println(longestOnes(test_data, 2))
 }
