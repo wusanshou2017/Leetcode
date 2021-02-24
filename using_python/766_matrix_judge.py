a1d3b8c52880d4871c8f999a19821a1e61d5fb32
wu_allin <252652905@qq.com>
2021-02-24 18:09:36 +0800

    848_solved

--- using_python/766_matrix_judge.py
+++ using_python/766_matrix_judge.py
@@ -1,15 +1,25 @@
-class Solution:
-    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        index_lst = []
+# class Solution:
+#     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+#         m = len(matrix)
+#         n = len(matrix[0])
+#         index_lst = []
 
-        # 主对角线
-        for i, j in zip(range(m), range(n)):
-            index_lst.append([i, j])
-            #  找子对角线
+#         # 主对角线
+#         for i, j in zip(range(m), range(n)):
+#             index_lst.append([i, j])
+#             #  找子对角线
 
-        for i in range(m - 1):
-            if matrix[i][:-1] != matrix[i + 1][1:]:
-                return False
-        return True
+#         for i in range(m - 1):
+#             if matrix[i][:-1] != matrix[i + 1][1:]:
+#                 return False
+#         return True
+
+
+
+
+
+s ="/home/tdwu/workspace/deployment/pull_keywords_deployment/models/ke1xun4jia1lian2.keywords.da4ping2yu3yin1diao4du4/"
+
+
+
+print(len(s))
