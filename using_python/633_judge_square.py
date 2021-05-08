f19732905c8c828728d00d24fa5e145cee62ab08
wu_allin <252652905@qq.com>
2021-05-08 15:55:39 +0800

    climb the mountain

--- using_python/633_judge_square.py
+++ using_python/633_judge_square.py
@@ -1,7 +1,24 @@
+# a ^ 2 + b ^ 2 == c ^ 2
 # 
-# a^2 +b^2 = c 
-# 
+# a^2 +2ab +b^2 = c^2+2ab 
+import math
 
 
 class Solution:
-    def judgeSquareSum(self, c: int) -> bool:
+    def isSquare(self, c: int):
+        srt = int(math.sqrt(c))
+        candidates = [num for num in range(0, srt + 1)]
+        left = 0
+        right = len(candidates) - 1
+
+        while left <= right:
+            num1 = candidates[left]
+            num2 = candidates[right]
+            if (num1 * num1 + num2 * num2) < c:
+                left += 1
+            elif (num1 * num1 + num2 * num2) == c:
+                return True
+            else:
+                right -= 1
+        return False
+
